583e5c56d23f9db506efc3800aece2df46222faa
jmcph4 <jack@jmcph4.dev>
2025-12-19 17:00:16 +1000

    Refine CLI

--- src/cli.rs
+++ src/cli.rs
@@ -6,10 +6,6 @@ use clap::Parser;
 pub struct Opts {
     /// Path to file to search
     pub haystack: PathBuf,
-    /// Search term (for exact full-text search)
-    #[clap(short, long, action)]
-    pub needle: Option<String>,
     /// Search prompt
-    #[clap(short, long, action)]
     pub prompt: String,
 }
